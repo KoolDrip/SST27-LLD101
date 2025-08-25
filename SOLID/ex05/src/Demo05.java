ab52b1da24e5928c39f9ace6718dcee60754fd0b
Jenish Thapa <thisisjenish05@google.com>
2025-08-25 11:49:09 +0530

    Refactored demo05

--- SOLID/ex05/src/Demo05.java
+++ SOLID/ex05/src/Demo05.java
@@ -1,10 +1,19 @@
-
 public class Demo05 {
-    static int areaAfterResize(Rectangle r){
-        r.setWidth(5); r.setHeight(4); return r.area();
+    static int areaAfterResize(Shape s) {
+        if (s instanceof Rectangle) {
+            Rectangle r = (Rectangle) s;
+            r.setWidth(5); r.setHeight(4);
+            return r.area();
+        } else if (s instanceof Square) {
+            Square sq = (Square) s;
+            sq.setSide(4);
+            return sq.area();
+        }
+        throw new IllegalArgumentException("Unknown shape");
     }
+
     public static void main(String[] args) {
         System.out.println(areaAfterResize(new Rectangle())); // 20
-        System.out.println(areaAfterResize(new Square()));    // 16 (!) violates expectation
+        System.out.println(areaAfterResize(new Square()));    // 16
     }
 }
--- SOLID/ex05/src/Square.java
+++ SOLID/ex05/src/Square.java
@@ -1,5 +1,8 @@
+public class Square implements Shape {
+    private int side;
 
-public class Square extends Rectangle {
-    @Override void setWidth(int w){ this.w = this.h = w; }
-    @Override void setHeight(int h){ this.w = this.h = h; }
+    public void setSide(int s) { this.side = s; }
+
+    @Override
+    public int area() { return side * side; }
 }
