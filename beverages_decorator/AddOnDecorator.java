d0ea6c6584fbff2afb0f13a71262ad9619c0c0a9
Jenish Thapa <thisisjenish05@gmail.com>
2025-09-08 16:25:43 +0530

    Added sol for Beverage Decorator

---
+++ beverages_decorator/AddOnDecorator.java
@@ -0,0 +1,12 @@
+package beverages_decorator;
+
+public abstract class AddOnDecorator extends Beverage {
+    protected Beverage beverage;
+
+    public AddOnDecorator(Beverage beverage) {
+        this.beverage = beverage;
+    }
+
+    @Override
+    public abstract int cost();
+}
--- beverages_decorator/Client.java
+++ beverages_decorator/Client.java
@@ -2,16 +2,22 @@ package beverages_decorator;
 
 public class Client {
 
-	public static void main(String[] args) {
-		
+    public static void main(String[] args) {
 
-		
-		Beverage coffee = new Cappuccino();
-        System.out.println("Coffee");
-		System.out.println(coffee.cost());
-		
-		
+        // Plain Cappuccino
+        Beverage coffee = new Cappuccino();
+        System.out.println("Cappuccino: " + coffee.cost());
 
-	}
+        // Cappuccino + Milk + Sugar
+        Beverage customCoffee = new Sugar(new Milk(new Cappuccino()));
+        System.out.println("Cappuccino + Milk + Sugar: " + customCoffee.cost());
 
+        // Latte + Whipped Cream + Sugar
+        Beverage latteWithToppings = new Sugar(new WhippedCream(new Latte()));
+        System.out.println("Latte + Whipped Cream + Sugar: " + latteWithToppings.cost());
+
+        // Latte + Double Milk
+        Beverage latteDoubleMilk = new Milk(new Milk(new Latte()));
+        System.out.println("Latte + Double Milk: " + latteDoubleMilk.cost());
+    }
 }
