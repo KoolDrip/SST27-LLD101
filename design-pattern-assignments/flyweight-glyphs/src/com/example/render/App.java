0b4d916c11fcde489a44a3619d54b1e68066987a
Jenish Thapa <thisisjenish05@gmail.com>
2025-09-08 12:23:16 +0530

    Added sol to DP Q5

--- design-pattern-assignments/flyweight-glyphs/src/com/example/render/App.java
+++ design-pattern-assignments/flyweight-glyphs/src/com/example/render/App.java
@@ -1,7 +1,18 @@
 package com.example.render;
+
 public class App {
     public static void main(String[] args) {
         Renderer r = new Renderer();
-        System.out.println("Cost=" + r.render("Hello Flyweight! ".repeat(2000)));
+        String text = "Hello Flyweight! ".repeat(2000);
+        int cost = r.render(text);
+        System.out.println("Cost=" + cost);
+
+        TextStyleFactory f = r.getStyleFactory();
+        TextStyle s1 = f.getTextStyle("Inter", 14, false);
+        TextStyle s2 = f.getTextStyle("Inter", 14, false);
+        TextStyle s3 = f.getTextStyle("Inter", 14, true);
+
+        System.out.println("s1 == s2 (identical config)? " + (s1 == s2));
+        System.out.println("s1 == s3 (different config)? " + (s1 == s3));
     }
 }
